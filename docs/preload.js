fb4d8ed987ba6fd434a4629d2cdbe9b867ad3e56
Claire Froelich <claire.froelich@gmail.com>
2025-12-03 20:59:47 -0500

    rm stamp

--- docs/preload.js
+++ docs/preload.js
@@ -1,5 +1,5 @@
 window.PRELOAD_MERCH_URLS = ["assets/merch/hoodie-santa.png","assets/merch/long-t-10.png","assets/merch/onesie-santa.png","assets/merch/pullover-santa.png","assets/merch/t-10.png","assets/merch/t-santa.png"];
-window.PRELOAD_STAMP_URLS = ["assets/stamps/s.png","assets/stamps/star.png"];
+window.PRELOAD_STAMP_URLS = ["assets/stamps/star.png"];
 
 // Function to draw the canvas background
 const drawBackground = (ctx, canvas) => {
